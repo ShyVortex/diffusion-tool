fdf8e74fe71456047c16243c8a44d781d64e5431
ShyVortex <angelotr@ik.me>
2024-07-30 17:46:11 +0200

    Implement stable-diffusion-3 pipeline

--- src/main/python/it/unimol/diffusiontool/generate_sd3.py
+++ src/main/python/it/unimol/diffusiontool/generate_sd3.py
@@ -1,5 +1,5 @@
 import sys
-from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
+from diffusers import StableDiffusion3Pipeline
 from PIL import Image
 from io import BytesIO
 import torch
@@ -10,7 +10,7 @@ import base64
 def main():
     # Check if the correct number of command-line arguments is provided
     if len(sys.argv) != 4:
-        print("Usage: python generate_pixelart.py <prompt> <tags> <date>")
+        print("Usage: python generate_sd3.py <prompt> <tags> <date>")
         sys.exit(1)
 
     # Get the prompt and date from the command-line arguments passed from Java
@@ -19,15 +19,29 @@ def main():
     date = sys.argv[3]
 
     # Model initialization and processing
-    repo_id = "kohbanye/pixel-art-style"
-    pipe = DiffusionPipeline.from_pretrained(repo_id, torch_dtype=torch.float16)
-    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-    pipe = pipe.to("cuda")
+    repo_id = "stabilityai/stable-diffusion-3-medium-diffusers"
+    pipe = StableDiffusion3Pipeline.from_pretrained(
+        repo_id,
+
+        # removes memory-intensive text encoder to decrease memory requirements
+        text_encoder_3=None,
+        tokenizer_3=None,
+
+        torch_dtype=torch.float16,
+    )
+
+    # offload components to CPU during inference to save memory
+    pipe.enable_model_cpu_offload()
 
     # Process the prompt and set the output path
     with torch.cuda.amp.autocast():
-        image = pipe(prompt=prompt, negative_prompt=tags, num_inference_steps=25).images[0]
-    output_folder = os.path.abspath("result/generated/pixelart")
+        image = pipe(
+            prompt=prompt,
+            negative_prompt=tags,
+            num_inference_steps=25,
+            guidance_scale=6.5
+        ).images[0]
+    output_folder = os.path.abspath("result/generated/sd3")
     output_filename = f"generated_image_{date}.png"
     output_filepath = os.path.join(output_folder, output_filename)
 
